9b5e5642108d6e9033a5422af49b4559668a321e
rafnaspp <rafnaspp4@gmail.com>
2025-01-01 11:11:58 +0530

    updates

--- src/pages/acertenityComponents/ForthSectionCard.jsx
+++ src/pages/acertenityComponents/ForthSectionCard.jsx
@@ -1,11 +1,9 @@
-"use client";
 import React from "react";
-import { MdOutlineForwardToInbox } from "react-icons/md";
-import { TbFidgetSpinner } from "react-icons/tb";
+import { Book, Heart, Brain, GraduationCap } from 'lucide-react';
 
 export const HeroParallax = () => {
   return (
-    <div className=" overflow-hidden antialiased relative flex flex-col">
+    <div className="overflow-hidden antialiased relative flex flex-col">
       <Header />
     </div>
   );
@@ -13,33 +11,31 @@ export const HeroParallax = () => {
 
 export const Header = () => {
   return (
-    <div className="max-w-5xl relative mx-auto  md:py-20 px-4 w-full">
+    <div className="max-w-5xl relative mx-auto md:py-20 px-4 w-full">
       <div className="space-y-16 md:space-y-24 font-bold w-full">
         <div className="flex flex-col md:flex-row w-full gap-14">
           {/* First Card */}
-          <div className="flex-1 gap-3 z-10 rounded-lg p-4 backdrop-blur-sm bg-opacity-50  flex flex-col items-center h-60">
+          <div className="flex-1 gap-3 z-10 rounded-lg p-4 backdrop-blur-sm bg-opacity-50 flex flex-col items-center h-60">
             <div className="flex flex-col items-center justify-center h-full">
-              {/* <MdOutlineForwardToInbox className="text-[#111111] w-16 h-16 flex-shrink-0  rounded-lg p-2" /> */}
-              <h1 className="text-lg md:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#20477a] to-[#2954E7]  mb-2 text-center">
+              <Heart className="text-[#20477a] w-16 h-16 flex-shrink-0 rounded-lg p-2" />
+              <h1 className="text-lg md:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#20477a] to-[#2954E7] mb-2 text-center">
                 Parent-Child Empowerment
               </h1>
-              <p className="max-w-2xl text-sm md:text-xl mt-2 text-[#20477a]  mb-8 text-center">
-                By involving parents in the journey, we foster a supportive
-                environment that nurtures academic and personal growth.
-              </p>
+              <p className="max-w-2xl text-sm md:text-xl mt-2 text-[#20477a] mb-8 text-center">
+              By actively involving parents in the journey, we foster a nurturing and supportive environment that empowers and nurtures both academic and personal growth.
+                            </p>
             </div>
           </div>
 
           {/* Second Card */}
-          <div className="flex-1 gap-3 z-10 rounded-lg p-4 backdrop-blur-sm bg-opacity-50  flex flex-col items-center h-60">
+          <div className="flex-1 gap-3 z-10 rounded-lg p-4 backdrop-blur-sm bg-opacity-50 flex flex-col items-center h-60">
             <div className="flex flex-col items-center justify-center h-full">
-              {/* <TbFidgetSpinner className="text-[#11111] w-16 h-16 flex-shrink-0  rounded-lg p-2" /> */}
+              <Brain className="text-[#20477a] w-16 h-16 flex-shrink-0 rounded-lg p-2" />
               <h1 className="text-lg md:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#20477a] to-[#2954E7] mb-2 text-center">
-                Psychological Support
+                Emotional and Social Development
               </h1>
-              <p className="max-w-2xl text-sm md:text-xl mt-2 text-[#20477a]  mb-8 text-center">
-                Our programs emphasize mental health and well-being, offering a
-                psychological
+              <p className="max-w-2xl text-sm md:text-xl mt-2 text-[#20477a] mb-8 text-center">
+                We focus on fostering emotional intelligence, helping children build healthy relationships, improve self-regulation, and grow into empathetic, confident individuals.
               </p>
             </div>
           </div>
@@ -47,31 +43,27 @@ export const Header = () => {
 
         <div className="flex flex-col md:flex-row w-full gap-14">
           {/* First Card */}
-          <div className="flex-1 gap-3 z-10 rounded-lg p-4 backdrop-blur-sm bg-opacity-50  flex flex-col items-center h-60">
+          <div className="flex-1 gap-3 z-10 rounded-lg p-4 backdrop-blur-sm bg-opacity-50 flex flex-col items-center h-60">
             <div className="flex flex-col items-center justify-center h-full">
-              {/* <MdOutlineForwardToInbox className="text-[#111111] w-16 h-16 flex-shrink-0  rounded-lg p-2" /> */}
-              <h1 className="text-lg md:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#20477a] to-[#2954E7]  mb-2 text-center">
-                Emotional and Social Development
+              <Book className="text-[#20477a] w-16 h-16 flex-shrink-0 rounded-lg p-2" />
+              <h1 className="text-lg md:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#20477a] to-[#2954E7] mb-2 text-center">
+                Psychological Support
               </h1>
-              <p className="max-w-2xl text-sm md:text-xl mt-2 text-[#20477a]  mb-8 text-center">
-                We focus on fostering emotional intelligence, helping children
-                build healthy relationships, improve self-regulation, and grow
-                into empathetic, confident individuals.
+              <p className="max-w-2xl text-sm md:text-xl mt-2 text-[#20477a] mb-8 text-center">
+                Our programs emphasize mental health and well-being, offering a psychological approach to support emotional balance, resilience, and growth.
               </p>
             </div>
           </div>
 
           {/* Second Card */}
-          <div className="flex-1 gap-3 z-10 rounded-lg p-4 backdrop-blur-sm bg-opacity-50  flex flex-col items-center h-60">
+          <div className="flex-1 gap-3 z-10 rounded-lg p-4 backdrop-blur-sm bg-opacity-50 flex flex-col items-center h-60">
             <div className="flex flex-col items-center justify-center h-full">
-              {/* <TbFidgetSpinner className="text-[#11111] w-16 h-16 flex-shrink-0  rounded-lg p-2" /> */}
+              <GraduationCap className="text-[#20477a] w-16 h-16 flex-shrink-0 rounded-lg p-2" />
               <h1 className="text-lg md:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#20477a] to-[#2954E7] mb-2 text-center">
                 Academic Excellence
               </h1>
-              <p className="max-w-2xl text-sm md:text-xl mt-2 text-[#20477a]  mb-8 text-center">
-                We empower children to achieve their highest potential by
-                fostering curiosity, critical thinking, and resilience in
-                academics and beyond.
+              <p className="max-w-2xl text-sm md:text-xl mt-2 text-[#20477a] mb-8 text-center">
+                We empower children to achieve their highest potential by fostering curiosity, critical thinking, and resilience in academics and beyond.
               </p>
             </div>
           </div>
--- src/pages/components/Courses.jsx
+++ src/pages/components/Courses.jsx
@@ -13,54 +13,62 @@ const animations = [
 ];
 
 export default function InfiniteCarousel() {
-
-  const [open , setOpen] = useState()
-  const [image , setImage] = useState()
+  const [open, setOpen] = useState();
+  const [image, setImage] = useState();
 
   const courses = [
     {
       id: 1,
       title: "Mindmap Jr",
-      description: "Mindmap Jr helps young learners develop problem-solving skills, confidence, and creativity.".padEnd(100, " "), // 100 characters
+      description:
+        "Mindmap Jr builds problem-solving, confidence, and creativity in young learners.",
       image: "./mindmap.jpg",
     },
     {
       id: 2,
       title: "Live Group Tuition",
-      description: "Live Group Tuition offers collaborative learning in a dynamic group environment, boosting confidence.".padEnd(100, " "), // 100 characters
+      description:
+        "Live Group Tuition offers learning in groups, boosting confidence and teamwork.",
       image: "./livecls.jpg",
     },
     {
       id: 3,
       title: "One-on-One Tuition",
-      description: "One-on-One Tuition provides personalized sessions focused on each student's unique needs and growth.".padEnd(100, " "), // 100 characters
+      description:
+        "One-on-One Tuition provides personalized sessions for individual student growth.",
       image: "./traffic.jpg",
     },
     {
       id: 4,
       title: "Smart Movers",
-      description: "Smart Movers teaches kids essential road safety skills for a safer future.".padEnd(100, " "), // 100 characters
+      description:
+        "Smart Movers teaches kids essential road safety skills for a safer future.",
       image: "./traffic1.jpg",
     },
     {
       id: 5,
       title: "Stage Stars",
-      description: "Stage Stars helps kids gain confidence and excel in public speaking, shining on stage.".padEnd(100, " "), // 100 characters
+      description:
+        "Stage Stars helps kids excel in public speaking and build confidence to shine.",
       image: "./publicspeak.jpg",
     },
     {
       id: 6,
       title: "Creative Hands",
-      description: "Creative Hands encourages kids to explore their creativity and develop artistic skills.".padEnd(100, " "), // 100 characters
+      description:
+        "Creative Hands inspires creativity and artistic skills in children through fun tasks.",
       image: "./creative.jpg",
     },
     {
       id: 7,
       title: "Math Whiz",
-      description: "Math Whiz helps kids unlock the magic of numbers and develop strong mathematical skills.".padEnd(100, " "), // 100 characters
+      description:
+        "Math Whiz introduces numbers and helps kids develop strong mathematical skills. ",
       image: "./math.jpg",
     },
-  ];
+];
+
+  
   
 
   const extendedCourses = [...courses, ...courses];
@@ -68,7 +76,7 @@ export default function InfiniteCarousel() {
   const settings = {
     slidesToShow: 4,
     slidesToScroll: 2,
-    autoplay: true, 
+    autoplay: true,
     autoplaySpeed: 3000,
     responsive: [
       {
@@ -90,67 +98,78 @@ export default function InfiniteCarousel() {
 
   return (
     <>
-    <div id='courses' className=" relative mt-10 py-12 bg-gradient-to-b from-white/20 via-gray-100 to-white/20 backdrop-blur-sm rounded-[40px] sm:rounded-[60px] mx-4 sm:mx-6 lg:mx-10 overflow-hidden">
-      <h2 className="text-3xl sm:text-4xl text-center font-bold mt-6 text-transparent bg-clip-text bg-gradient-to-r from-[#20477a] to-[#2954E7] mb-6">
-        We Offer An Exclusive
-        <br />
-        Program For Kids
-      </h2>
-      <h4 className="text-lg sm:text-xl text-center text-transparent bg-clip-text bg-gradient-to-r from-[#20477a] to-[#2954E7] font-semibold mx-4 sm:mx-10 mb-10">
-        Enroll now to map young minds for a bright future! Mindmap Jr builds
-        confidence, essential habits, and problem-solving skills, setting the
-        foundation for success.
-      </h4>
+      <div
+        id="courses"
+        className=" relative mt-10 py-12 bg-gradient-to-b from-white/20 via-gray-100 to-white/20 backdrop-blur-sm rounded-[40px] sm:rounded-[60px] mx-4 sm:mx-6 lg:mx-10 overflow-hidden"
+      >
+        <h2 className="text-3xl sm:text-4xl text-center font-bold mt-6 text-transparent bg-clip-text bg-gradient-to-r from-[#20477a] to-[#2954E7] mb-6">
+          We Offer An Exclusive
+          <br />
+          Program For Kids
+        </h2>
+        <h4 className="text-lg sm:text-xl text-center text-transparent bg-clip-text bg-gradient-to-r from-[#20477a] to-[#2954E7] font-semibold mx-4 sm:mx-10 mb-10">
+          Enroll now to map young minds for a bright future! Mindmap Jr builds
+          confidence, essential habits, and problem-solving skills, setting the
+          foundation for success.
+        </h4>
 
-      <div>
-        <Slider {...settings}>
-          {extendedCourses.map((course, index) => (
-            <div
-              key={`${course.id}-${index}`}
-              className={`flex flex-col justify-between sm:w-80 h-[420px] bg-white rounded-xl shadow-lg overflow-hidden ${
-                animations[index % animations.length]
-              }`}
-            >
-              <div>
-                <img
-                  src={course.image}
-                  alt={course.title}
-                  className="w-full h-44 sm:h-48 lg:h-52 object-cover rounded-t-xl"
-                />
-                <div className="p-4">
-                  <h3 className="text-base sm:text-lg font-semibold">
-                    {course.title}
-                  </h3>
-                  <p className="mt-2 text-sm text-gray-600">
-                    {course?.description}
-                  </p>
-                </div>
-              </div>
-              <div className="p-4">
-                <motion.button
-                  whileHover={{ scale: 1.05 }}
-                  whileTap={{ scale: 0.95 }}
-                  onClick={()=>{
-                    setOpen(true)
-                    setImage(course.image)
-                  }}
-                  className="w-full sm:w-auto px-4 sm:px-6 py-2 rounded-full text-sm font-medium text-white bg-gradient-to-r from-blue-500 to-blue-900 hover:from-indigo-500 hover:to-blue-500"
-                  >
-                  More info
-                </motion.button>
-              </div>
-            </div>
-          ))}
-        </Slider>
-      </div>
+        <div>
+          <Slider {...settings}>
+            {extendedCourses.map((course, index) => (
+             <div
+             key={`${course.id}-${index}`}
+             className={`flex flex-col justify-between sm:w-80 h-[420px] bg-white rounded-xl shadow-lg overflow-hidden ${
+               animations[index % animations.length]
+             } relative`} // Added relative positioning to the parent
+           >
+             <div>
+               <img
+                 src={course.image}
+                 alt={course.title}
+                 className="w-full h-44 sm:h-48 lg:h-52 object-cover rounded-t-xl"
+               />
+               <div className="p-4">
+                 <h3 className="text-base sm:text-lg font-semibold">
+                   {course.title}
+                 </h3>
+                 <p className="mt-2 text-sm text-gray-600">
+                   {course?.description}
+                 </p>
+               </div>
+             </div>
+           
+             {/* Button section */}
+             <div className="absolute bottom-2 left-0 w-full p-4 flex justify-start">  {/* Added absolute positioning and fixed bottom */}
+               <motion.button
+                 whileHover={{ scale: 1.05 }}
+                 whileTap={{ scale: 0.95 }}
+                 onClick={() => {
+                   setOpen(true);
+                   setImage(course.image);
+                 }}
+                 className="w-full sm:w-auto px-4 sm:px-6 py-2 rounded-full text-sm font-medium text-white bg-gradient-to-r from-blue-500 to-blue-900 hover:from-indigo-500 hover:to-blue-500"
+               >
+                 More info
+               </motion.button>
+             </div>
+           </div>
+           
+            ))}
+          </Slider>
+        </div>
 
-      {/* <div className="flex justify-center mt-8">
+        {/* <div className="flex justify-center mt-8">
         <button className="px-4 sm:px-6 py-2 sm:py-3 bg-[#2954E7] text-white font-semibold rounded-full hover:bg-indigo-700 transition-colors duration-300">
           View All Programs
         </button>
       </div> */}
-    </div>
-    <SplitPopupManual typeOflead={`courses - ${image}`} open={open} setOpen={setOpen} imageShow={image} />
+      </div>
+      <SplitPopupManual
+        typeOflead={`courses - ${image}`}
+        open={open}
+        setOpen={setOpen}
+        imageShow={image}
+      />
     </>
   );
 }
